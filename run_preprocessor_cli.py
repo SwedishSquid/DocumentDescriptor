c862e64beadbbac0693e1b4fd69cfb85eed3be60
Alexey_Dzhevello <fishwere@mail.ru>
2024-01-09 19:23:53 +0500

    fixed prep cli start

--- run_preprocessor_cli.py
+++ run_preprocessor_cli.py
@@ -2,4 +2,4 @@ import sys
 from app.preprocessor_cli import PreprocessorCLI
 
 if __name__ == '__main__':
-    PreprocessorCLI().run(['prep', '-p', r'E:\ProjectLib\result_root', 'start'])#sys.argv)
+    PreprocessorCLI().run(sys.argv)
